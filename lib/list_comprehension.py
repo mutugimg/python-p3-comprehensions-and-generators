9f96addd3eea25fb36e9ac79d5f92a02da67cdb7
Mark Mutugi <mutugimg@gmail.com>
2023-10-19 17:53:06 +0300

    Completed Assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    num_list_even = [numbers for numbers in num_list if numbers % 2 == 0]
+    # print(num_list_even)
+    return num_list_even
+# num_list = range(17)
+# return_evens(num_list)
 
 def make_exclamation(sentence_list):
-    pass
+    sentences = [sentc + "!" for sentc in sentence_list]
+    return sentences
+
+sentence_list = list()
+make_exclamation(sentence_list)
